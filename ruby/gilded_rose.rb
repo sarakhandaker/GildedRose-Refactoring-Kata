f241b736f731157976157c8559015b8e3ca179b5
SaraKhandaker <kttsara@gmail.com>
2020-08-19 14:21:31 -0700

    extracted quality up and down

--- ruby/gilded_rose.rb
+++ ruby/gilded_rose.rb
@@ -12,27 +12,33 @@ class GildedRose
 
   def update_item(item)
     return if item.name == "Sulfuras, Hand of Ragnaros"
-    if item.name != "Aged Brie" && item.name != "Backstage passes to a TAFKAL80ETC concert" && item.quality > 0  
-      item.quality -=1  if item.name.split(" ")[0] == "Conjured"
-      item.quality -=1
+    if item.name != "Aged Brie" && item.name != "Backstage passes to a TAFKAL80ETC concert"  
+      change_quality(item, "decrease")  if item.name.split(" ")[0] == "Conjured"
+      change_quality(item, "decrease")
     else
-    if item.quality < 50 && item.quality > 0  
-      item.quality+=1
+      change_quality(item, "increase")
       if item.name == "Backstage passes to a TAFKAL80ETC concert"
-        item.quality +=1 if item.sell_in < 11 && item.quality < 50
-        item.quality +=1 if item.sell_in < 6 && item.quality < 50
+        change_quality(item, "increase") if item.sell_in < 11
+        change_quality(item, "increase") if item.sell_in < 6
       end
     end
-    end
-    item.sell_in = item.sell_in - 1
+    item.sell_in -= 1
     if item.sell_in < 0
       if item.name == "Aged Brie"
-        item.quality +=1 if item.quality < 50
+        change_quality(item, "increase")
       else 
-        item.quality -=1 if item.quality > 0
-        item.quality -=1 if item.name.split(" ")[0] == "Conjured" && item.quality > 0
+        change_quality(item, "decrease")
+        change_quality(item, "decrease") if item.name.split(" ")[0] == "Conjured"
         item.quality = 0 if item.name == "Backstage passes to a TAFKAL80ETC concert"
       end 
+    end
+  end
+
+  def change_quality(item, direction)
+    if direction == "increase"
+      item.quality +=1 if item.quality < 50
+    else 
+      item.quality -=1 if item.quality >0
     end
   end
 
